a16659f0c0fd9505d4f3dae3d198a8b18ba6985a
xrotwang <xrotwang@googlemail.com>
2019-05-21 14:01:56 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyclics-clustering',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="clustering algorithms for CLICS networks",
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
