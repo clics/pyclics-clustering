a38c84bd30e768abf6a14fc40fda2bb56d231b42
xrotwang <xrotwang@googlemail.com>
2019-05-21 14:00:48 +0200

    release 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyclics-clustering',
-    version='0.1',
+    version='1.0.0',
     description="clustering algorithms for CLICS networks",
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
